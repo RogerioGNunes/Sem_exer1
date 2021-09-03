99bb59fa045cf89d8a159a8ac092d0c5d546fd86
RogerioGNunes <rg.nunes2013@gmail.com>
2021-09-03 00:14:17 +0000

    exer1 da semana 5

---
+++ exer1.java
@@ -0,0 +1,10 @@
+public class exer1 {
+    public static void main(String[]args){
+        long fatorial=1;
+        for(int f=40;f>=1;f--){
+            fatorial=f*fatorial;
+        }
+        System.out.println(fatorial);
+    }
+
+}
